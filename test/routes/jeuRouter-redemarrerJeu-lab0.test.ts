8f04455455587257527cf1311d77b3e83d07cbd2
RiddimCrunch <samuel.cossette.1@ens.etsmtl.ca>
2025-09-15 19:22:17 -0400

    Étape 3-4 Redémarrer

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,30 @@
 // Vous devez insérer les nouveaux tests ici
+import supertest from 'supertest';
 import { assert } from 'console';
 import 'jest-extended';
+import app from '../../src/app';
+import { jeuRoutes } from "../../src/routes/jeuRouter";
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Pierre';
+
+beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
+    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom2 });
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it("Redémarrer ", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  it("Il n'y a plus de joueur", async () => {
+    const joueursJSON = jeuRoutes.controleurJeu.joueurs;
+    const joueursArray = JSON.parse(joueursJSON);
+    expect(joueursArray.length).toBe(0);
   });
 });
